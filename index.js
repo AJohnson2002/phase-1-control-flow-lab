a2559ca5c40fa00e17f61027048d16cee02e2fac
Averette <averettewjohnson@gmail.com>
2023-06-29 13:14:57 -0500

    completed

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let ride
+  if (someNumber <= 400){
+    ride = ("This one is on me!");
+  } else if (someNumber > 400 && someNumber < 2000) {
+    ride = ("That will be twenty bucks.")
+  } else if (someNumber > 2000 && someNumber < 2500) {
+    ride = ("I will gladly take your thirty bucks.")
+  } else if (someNumber > 2500){
+    ride = ("No can do.")
+  }
+  return ride;
+}
+const city = "NYC";
+function ternaryCheckCity(city){
+  return (city == "NYC") ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+const tip = 'generous';
+ function switchOnCharmFromTip(tip) {
+   switch (tip) {
+     case 'generous':
+     return ('Thank you so much.');
+     
+     
+     case 'not as generous':
+     return ('Thank you.');
+     
+     
+     default:
+     return ('Bye.');
+   }
+ }
